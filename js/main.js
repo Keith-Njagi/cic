17446d9cc2e3d8c38f1a9c7ff97baf0a2b53801d
Keith-Njagi <keithgatana@gmail.com>
2019-09-16 07:22:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    // Responsive Top Nav-bar Slide Toggle
+    $('.nav-button-light').click(function () {
+        $('.nav-menu li a').slideToggle(200)
+    })
+    //Toggle between port items(developer and drummer)
+    $('.port-item').click(function () {
+        $('.collapse').collapse('hide');
+    });
+            
+    $(document).on('click', '[data-toggle="lightbox"]', function (e) {
+        e.preventDefault();
+        $(this).ekkoLightbox();
+    });
+})
