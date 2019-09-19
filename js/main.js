345b8b02e727388bfb2e000fcae65851ae8dfdb3
Keith-Njagi <keithgatana@gmail.com>
2019-09-19 10:44:53 +0300

    animations added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-$(function () {
-    // Responsive Top Nav-bar Slide Toggle
-    $('.nav-button-light').click(function () {
-        $('.nav-menu li a').slideToggle(200)
-    })
-    //Toggle between port items(developer and drummer)
-    $('.port-item').click(function () {
-        $('.collapse').collapse('hide');
-    });
-            
-    $(document).on('click', '[data-toggle="lightbox"]', function (e) {
-        e.preventDefault();
-        $(this).ekkoLightbox();
-    });
+jQuery(document).ready(function ($) {
+  // Responsive Top Nav-bar Slide Toggle
+  $('.nav-button-light').click(function () {
+    $('.nav-menu li a').slideToggle(200)
+  })
+  // Toggle between port items(developer and drummer)
+  $('.port-item').click(function () {
+    $('.collapse').collapse('hide');
+  });
+
+  $(document).on('click', '[data-toggle="lightbox"]', function (e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
 })
