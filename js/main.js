233e28279095f90d94ad9804aaefea62e384849a
Keith-Njagi <keithgatana@gmail.com>
2019-09-27 10:54:32 +0300

    team + services changes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-jQuery(document).ready(function ($) {
+JQuery(document).ready(function ($) {
+  // Services toggle
+  $('#audit-assur').click(function () {
+    $('.collapse').collapse('toggle')
+  })
+
   // Responsive Top Nav-bar Slide Toggle
   $('.nav-button-light').click(function () {
     $('.nav-menu li a').slideToggle(200)
   })
   // Toggle between port items(developer and drummer)
   $('.port-item').click(function () {
-    $('.collapse').collapse('hide');
-  });
+    $('.collapse').collapse('hide')
+  })
 
   $(document).on('click', '[data-toggle="lightbox"]', function (e) {
-    e.preventDefault();
-    $(this).ekkoLightbox();
-  });
+    e.preventDefault()
+    $(this).ekkoLightbox()
+  })
+  // Services Block
+  $('.service-item-block').click(function () {
+    $('.service-collapse').addClass('.service-block').removeClass('.service-collapse')
+  })
+  // Services Collapse
+  $('.service-item-block').click(function () {
+    $('.service-block').addClass('.service-collapse').removeClass('.service-block')
+  })
 })
